942c1e6ec1fac81972a7e5021a1b6ea3bed2566b
Mohammed Salim <50304249+HazyAutumnRain@users.noreply.github.com>
2023-01-26 17:30:16 +0400

    world optimization

--- main.lua
+++ main.lua
@@ -1,17 +1,29 @@
 require 'player'
+require 'world/room_1'
+require 'world/room_2'
 
 Game = {}
 
 function love.load()
 	love.graphics.setDefaultFilter("nearest", "nearest")
-	Game.world = {
-		{2,2,2,2,2,2},
+	Room_1:load()
+	Room_2:load()
+
+	Game.room_3 = {
+		{2,2,3,3,2,2},
 		{2,0,0,0,0,2},
 		{2,0,0,0,0,2},
 		{2,0,0,0,0,2},
 		{2,0,0,0,0,2},
 		{2,2,2,2,2,2}
 	}
+	
+	Game.world = {
+		{Room_2, Room_1},
+		{Game.room_3}
+	}
+	Game.currentRoomX = 2
+	Game.currentRoomY = 1
 	Game.scale = 15
 	Player:load()
 end
@@ -20,24 +32,38 @@ function love.update(dt)
 end
 
 function love.draw()
-	for y = 1, 6, 1 do
-		for x = 1, 6, 1 do
-			drawing_world(x,y)
+
+	-- Choosing the current room
+	local room = Game.world[Game.currentRoomY][Game.currentRoomX]
+	local room_layout = room.room
+	local width, height = room.width, room.height
+
+	for y = 1, height, 1 do
+		for x = 1, width, 1 do 
+			drawing_world(room_layout,x,y)
 		end
 	end
 end
 
-function drawing_world(x,y)
+function drawing_world(room,x,y)
 	-- Setting colors
 	local wall = {78,136,173}
 
-	local unit = Game.world[x][y]
+	local unit = room[y][x]
 	if unit == 2 then
 		love.graphics.setColor(love.math.colorFromBytes(wall))
 		love.graphics.print('#', x * Game.scale, y * Game.scale)
 	elseif unit == 0 then
 		love.graphics.setColor(1,1,1,0.3)
 		love.graphics.print('.', x * Game.scale, y * Game.scale)
+	elseif unit == 3 then
+		if (y == 1 or y == 6) then
+			love.graphics.setColor(love.math.colorFromBytes(wall))
+			love.graphics.print('-', x * Game.scale, y * Game.scale)
+		else
+			love.graphics.setColor(love.math.colorFromBytes(wall))
+			love.graphics.print('|', x * Game.scale, y * Game.scale)
+		end
 	end
 
 	Player:draw()
--- player.lua
+++ player.lua
@@ -14,37 +14,65 @@ function Player:draw()
 end
 
 function Player:keypressed(key)
+	-- Get the current room
+	local current_room = Game.world[Game.currentRoomY][Game.currentRoomX].room
+	local current_room_width, current_room_height = Game.world[Game.currentRoomY][Game.currentRoomX].width, Game.world[Game.currentRoomY][Game.currentRoomX].height
+
 	-- Handle Movement
 
 	-- Right Movement
 	if key == 'd' then
 		-- Checking if something is in the way
-		if Game.world[self.x+1][self.y] == 0 then
+		if current_room[self.y][self.x+1] ~= 2 then
 			self.x = self.x + 1
+		end
+
+		-- Handle room transtion
+		if (current_room[self.y][self.x] == 3) then
+			Game.currentRoomX = Game.currentRoomX + 1
+			self.x = 2
 		end
 	end
 
 	-- Left Movement
 	if key == 'a' then
 		-- Checking if something is in the way	
-		if Game.world[self.x-1][self.y] == 0 then
+		if current_room[self.y][self.x-1] ~= 2 then
 			self.x = self.x - 1
+		end
+
+		-- Handle room transition
+		if (current_room[self.y][self.x] == 3) then
+			Game.currentRoomX = Game.currentRoomX - 1
+			self.x = current_room_width+1
 		end
 	end
 
 	-- Up Movement
 	if key == 'w' then
 		-- Checking if something is in the way
-		if Game.world[self.x][self.y-1] == 0 then
+		if current_room[self.y-1][self.x] ~= 2 then
 			self.y = self.y - 1
+		end
+
+		-- Handle room transition
+		if (current_room[self.y][self.x] == 3) then
+			Game.currentRoomY = Game.currentRoomY - 1
+			self.y = 5
 		end
 	end
 
 	-- Down Movement
 	if key == 's' then
 		-- Checking if something is in the way
-		if Game.world[self.x][self.y+1] == 0  then
+		if current_room[self.y+1][self.x] ~= 2  then
 			self.y = self.y + 1
+		end
+
+		-- Handle room transition
+		if current_room[self.y][self.x] == 3 then
+			Game.currentRoomY = Game.currentRoomY + 1
+			self.y = 2
 		end
 	end
 end
